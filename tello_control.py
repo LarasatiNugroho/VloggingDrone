5e42283e2d97a356bd19007e25eafeda82b21167
LarasatiNugroho <anlarasati11@gmail.com>
2021-11-24 15:24:40 +0700

    revisi definisi velocity

--- tello_control.py
+++ tello_control.py
@@ -29,18 +29,19 @@ def TrackFace(drone,info,img):
 
         print(f"Found a face")
         # for-backward velocity
-        velocity = int((3000 - info[1]) * 0.05)
-        velocity = int(np.clip(velocity, -10, 10))
+        error[0] = int(15000-info[1])
+        velocity = pid_fb[0]*error[0] + pid_fb[1]*(error[0]-p_error[0]) + pid_fb[2]*(error[0]+p_error[0])
+        p_error[0] = error[0]
 
         # yaw velocity
-        error = info[0][0] - img.shape[1] // 2  # if error positive (+yaw velocity) and vice versa
-        velocity1 = int(0.4 * error)
-        velocity1 = int(np.clip(velocity1, -10, 10))
+        error[1] = int(info[0][0]-img.shape[1]//2)
+        velocity1 = pid_yaw[0] * error[1] + pid_yaw[1] * (error[1] - p_error[1]) + pid_yaw[2] * (error[1] + p_error[1])
+        p_error[1] = error[1]
 
         # up down velocity
-        error2 = info[0][1] - img.shape[0] // 2  # img.shape[0] // 2
-        velocity2 = int(0.4 * error2)
-        velocity2 = int(np.clip(velocity2, -10, 10))
+        error[2] = int(info[0][1] - img.shape[0] // 2)
+        velocity2 = pid_ud[0] * error[2] + pid_ud[1] * (error[2] - p_error[2]) + pid_ud[2] * (error[2] + p_error[2])
+        p_error[2] = error[2]
     else:
         print(f"Drone is searching for a face")
         velocity = 0
