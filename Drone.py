3e70fb0337fc0bb73b0202899e54a5a40c8024b5
LarasatiNugroho <anlarasati11@gmail.com>
2021-11-24 16:49:35 +0700

    hover

--- Drone.py
+++ Drone.py
@@ -1,22 +1,20 @@
 from tello_cv import *
 from tello_control import *
-
-#initial condition
-pid_fb = [1,1.6,0]
-pid_yaw = [0.2,0.5,0]
-pid_ud = [0.4,2,0]
-error = [0,0,0] #fb,yaw,ud
-p_error = [0,0,0] #fb, yaw, ud
+import time
 
 #start here
 counter = 0
 drone = connectingTello()                       #as the name stated
-while True:
-    #drone takes flight
-    if counter == 0:
-        #drone.takeoff()
-        counter == 1
 
+#hover drone
+time.sleep(5)
+drone.takeoff()
+time.sleep(3)
+drone_height = drone.get_height()
+print(f"my height is {drone_height}")
+drone.move_up(int(150-drone_height))
+
+while True:
     img = TelloGetFrame(drone)              #Capture picture from the drone's camera
     img,info= FaceDetection(img)               #Detect a face in the image; info shows [cx,cy] and [area face rectangle]
     velocity_maju,velocity_yaw,velocity_up = TrackFace(drone,info,img)     #FaceTracking
@@ -26,7 +24,10 @@ while True:
 
     # stop when press "k"
     if cv2.waitKey(1) & 0xFF == ord('k'):
-        #drone.land()
+        drone.land()
         break
 
+drone.streamoff()
 cv2.destroyAllWindows()
+
+#ini revisi takeoffnya nggak loop
